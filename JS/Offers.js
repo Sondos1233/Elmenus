399b5ca0564dc6f165fbb188f6cffe5c4fa97d53
Rami Ayayd <engramiahmad97@gmail.com>
2022-01-15 21:14:07 +0200

    Last commit before quota ended :cry:

--- JS/Offers.js
+++ JS/Offers.js
@@ -2,10 +2,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-app.js";
 import {
   getFirestore,
-  getDocs,getDoc,
-  collection,doc
-  ,limit,query
+  getDocs,
+  getDoc,
+  collection,
+  doc,
+  limit,
+  query,
 } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-firestore.js";
+import {
+  getStorage,
+  ref,
+  getDownloadURL
+} from "https://www.gstatic.com/firebasejs/9.6.2/firebase-storage.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,11 +25,14 @@ const firebaseConfig = {
   storageBucket: "elmenusclone.appspot.com",
   messagingSenderId: "57271621220",
   appId: "1:57271621220:web:ba8d19b9fddb0988db7171",
+  storageBucket: "gs://elmenusclone.appspot.com/",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
+const storage = getStorage(app);
+const storageRef = ref(storage);
 
 //Fetching data
 getDocs(collection(firestore,"Restaurant")).then((restaurantCollectoin)=>{  //restaurantCollectoin is array of all documents
@@ -30,46 +41,52 @@ getDocs(collection(firestore,"Restaurant")).then((restaurantCollectoin)=>{  //re
             offersCollectoin.forEach((docsInOfferstCollectoin) => {
                 console.log(docsInOfferstCollectoin.data().Description)
                 if(docsInOfferstCollectoin.data().Description){
-                    document.getElementById("cardsArea").innerHTML+=` <div class="col" id="wholeCard">
-                    <a href="#">
-                        <div class="card h-100">
-                            <img src="https://s3-eu-west-1.amazonaws.com/elmenusv5-stg/Normal/9b148a80-c4fa-4606-9293-e3cae4e2990e.jpg"
-                                class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <div id="upperPart">
-                                    <img src="https://s3-eu-west-1.amazonaws.com/elmenusv5-stg/Thumbnail/f352d864-b63c-482a-b584-58bda87affca.jpg"
-                                        alt="" srcset="">
-                                    <div id="restName">
-                                        <div id="offerDisc">offerDisc</div>
-                                        <h3>${docsInRestaurantCollectoin.data().ResName}</h3>
-                                        <span>${docsInRestaurantCollectoin.data().Type}</span>
-                                    </div>
-                                    <div id="rating">
-                                        <span>
-                                            <i class="far fa-star"></i>
-                                            <i class="far fa-star"></i>
-                                            <i class="far fa-star"></i>
-                                            <i class="far fa-star"></i>
-                                            <i class="far fa-star"></i>
-                                            <span>(316)</span>
-                                        </span>
+                    getDownloadURL(ref(storage, `ResImges/${docsInRestaurantCollectoin.data().ResName}/Logo_${docsInRestaurantCollectoin.data().ResName}.jpg`)).then((url1) => {
+                        getDownloadURL(ref(storage, `ResImges/${docsInRestaurantCollectoin.data().ResName}/Atract_${docsInRestaurantCollectoin.data().ResName}.jpg`)).then((url2) => {
+                                document.getElementById("cardsArea").innerHTML+=` <div class="col" id="wholeCard">
+                                <a href="#">
+                                    <div class="card h-100">
+                                        <img id="bigImg" src="${url2}"
+                                            class="card-img-top" alt="...">
+                                        <div class="card-body">
+                                            <div id="upperPart">
+                                                <img src="${url1}"
+                                                    alt="" srcset="">
+                                                <div id="restName">
+                                                    <div id="offerDisc">offerDisc</div>
+                                                    <h3>${docsInRestaurantCollectoin.data().ResName}</h3>
+                                                    <span>${docsInRestaurantCollectoin.data().Type}</span>
+                                                </div>
+                                                <div id="rating">
+                                                    <span>
+                                                        <i class="far fa-star"></i>
+                                                        <i class="far fa-star"></i>
+                                                        <i class="far fa-star"></i>
+                                                        <i class="far fa-star"></i>
+                                                        <i class="far fa-star"></i>
+                                                        <span>(316)</span>
+                                                    </span>
 
-                                        <span id="criditCardIcon"><i class="fa fa-credit-card"></i></span>
+                                                    <span id="criditCardIcon"><i class="fa fa-credit-card"></i></span>
+                                                </div>
+                                            </div>
+                                            <div id="midPart">
+                                                <i class="fa fa-percentage"></i>
+                                                <span>${docsInOfferstCollectoin.data().Description}</span>
+                                            </div>
+                                            <div id="lowerPart">
+                                                <span><i class="fa fa-motorcycle"></i>50 mins</span>
+                                                <span><i class="fa fa-circle"></i>ORDER ONLINE</span>
+                                                <span><i class="fa fa-map-marked-alt"></i>Live Tracking</span>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                                <div id="midPart">
-                                    <i class="fa fa-percentage"></i>
-                                    <span>${docsInOfferstCollectoin.data().Description}</span>
-                                </div>
-                                <div id="lowerPart">
-                                    <span><i class="fa fa-motorcycle"></i>50 mins</span>
-                                    <span><i class="fa fa-circle"></i>ORDER ONLINE</span>
-                                    <span><i class="fa fa-map-marked-alt"></i>Live Tracking</span>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>`
+                                </a>
+                            </div>`
+                        })    
+
+
+                    })    
                 }
 
 
